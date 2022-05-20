61896728b969de5d1c51372daf870c3ca5a3df94
Daniel Kleinert <daniel@pristinebytes.com>
2022-05-20 14:48:21 +0200

    add more light

--- pibooth_flash.py
+++ pibooth_flash.py
@@ -1,8 +1,12 @@
 import pibooth
 import RPi.GPIO as GPIO
+from phue import Bridge
 
 __version__ = "1.0.0"
 RELAIS_1_GPIO = 17
+
+hue = Bridge('192.168.178.174')
+
 
 @pibooth.hookimpl
 def pibooth_startup(app):
@@ -11,10 +15,12 @@ def pibooth_startup(app):
 
 @pibooth.hookimpl
 def state_preview_enter(app):
+    hue.set_light(3, {'transitiontime': 0, 'on': True, 'bri': 254})
     GPIO.output(RELAIS_1_GPIO, GPIO.LOW)
 
 @pibooth.hookimpl
 def state_capture_exit(app):
+    hue.set_light(3, {'transitiontime': 0, 'on': False})
     GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
 
 
